a9efa10df59d39d52d25bc18f9a4ba75efc4315c
Creatlydev <creatlydev@gmail.com>
2023-07-16 10:02:43 -0500

    Primera version nuevo Challenge Edie Homepage

---
+++ Responsive Web Developer/edie-homepage-master/src/js/events.js
@@ -0,0 +1,9 @@
+let icon_menu = document.querySelector(".icon-container");
+let navbar = document.querySelector(".nav__content");
+
+icon_menu.addEventListener("click", () => {
+  navbar.classList.toggle("nav__content_open");
+});
+
+
+
